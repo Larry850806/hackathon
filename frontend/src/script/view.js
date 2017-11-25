05240dc3b7a63a78e79dc7d9778a598c4c0cb478
Larry Lu <pudding850806@gmail.com>
2017-11-25 14:19:57 +0800

    Use link to load highcharts

--- frontend/src/script/view.js
+++ frontend/src/script/view.js
@@ -1,8 +1,10 @@
-import Highcharts from 'highcharts'
+// import Highcharts from 'highcharts'
+// window.Highcharts = Highcharts
+// require('highcharts-more')
 import config from './config'
 
 function showBubbleChart() {
-    Highcharts.chart('container',config)
+  Highcharts.chart('container', config)
 }
 
-export default {showBubbleChart}
+export default { showBubbleChart }
