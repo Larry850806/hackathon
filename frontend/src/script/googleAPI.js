82f54dfabca820a8ec7c3a1be4bcee2d635ea547
Larry Lu <pudding850806@gmail.com>
2017-11-25 13:35:10 +0800

    Fix bug and change zoom level to 12

--- frontend/src/script/googleAPI.js
+++ frontend/src/script/googleAPI.js
@@ -1,25 +1,25 @@
 import datas from '../../location.json'
 
 function initMap() {
-    return new Promise((resolve, reject) => {
-        let map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 2,
-            center: { lat: 24.993113, lng: 121.301028 },
-            mapTypeId: 'terrain'
-        })
-        resolve(map)
+  return new Promise((resolve, reject) => {
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: { lat: 24.993113, lng: 121.301028 },
+      mapTypeId: 'terrain',
     })
+    resolve(map)
+  })
 }
 
 function heapmap(map) {
-    return new Promise((resolve, reject) => {
-        const heapmap = new google.maps.visualization.HeatmapLayer({
-            data: datas.map(data => new google.maps.LatLng(data.lat, data.lon)),
-            dissiapting: false,
-            map,
-        })
-        resolve(heapmap)
+  return new Promise((resolve, reject) => {
+    const heapmap = new google.maps.visualization.HeatmapLayer({
+      data: datas.map(data => new google.maps.LatLng(data.lat, data.lon)),
+      dissiapting: false,
+      map,
     })
- }
+    resolve(heapmap)
+  })
+}
 
- export default {initMap, heapmap}
+export default { initMap, heapmap }
